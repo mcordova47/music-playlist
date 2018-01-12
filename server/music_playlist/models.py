5e2f48aefe92e186fb195230ba9d2fff30ff6542
Marc Cordova <mcordova47@gmail.com>
2018-01-11 20:01:41 -0500

    refactor: change song notes to TextField and allow blanks

--- server/music_playlist/models.py
+++ server/music_playlist/models.py
@@ -7,7 +7,7 @@ class Song(models.Model):
     artist = models.CharField(max_length=200)
     year = models.IntegerField()
     album = models.CharField(max_length=200)
-    notes = models.CharField(max_length=2000)
+    notes = models.TextField(blank=True)
 
     def __unicode__(self):
         return 'Song: ' + self.title
