2825d32d48ba8607e6213fcbcc56d33e778a3d2d
mcordova47@gmail.com <mcordova47@gmail.com>
2018-05-06 15:59:56 -0400

    refactor: replace server with ruby server

--- server/mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-from music_playlist.views import songs
-
-urlpatterns = [
-    url(r'^api/songs/$', songs),
-    url(r'^admin/', include(admin.site.urls)),
-]
